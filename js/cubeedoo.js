b5aaffa047d2b9c6c4bf06c359be8c1ee383c9ee
Estelle Weyl <estelle@weyl.org>
2011-10-13 15:16:32 -0700

    notes on code order

--- js/cubeedoo.js
+++ js/cubeedoo.js
@@ -1,4 +1,4 @@
-var cube = {
+var qbdoo = {
 	currentLevel: 1,
 	currentTheme: "numbers",
 	defaultGameDuration: 120,
@@ -26,16 +26,46 @@ var cube = {
     //accepts (none | start | stop)
   },
   pause: function() {
-    
+    // use dataset to get value for all the cards.
+	
+	// add theme to value set
+	
+	// add level to value set
+	
+	// add timeleft to value set
+	
+	// add score to value set
+	
+	// add to local storage
+
+	// kill the timer
+	
+	// return
+  },
+  playGame: function(){
+	// get local storage
+	
+	
+	// set the theme
+	
+	// set the level
+	
+	// set the 24 cards
+	
+	// set the score back
+	
+	// start the time
+	  
   },
   renderMenu: function() {
     
   },
-  changeTheme: function() {
-    
+  changeTheme: function(class) {
+	//change the theme by changing the class
+    document.getElementById('game').setAttribute('class',class);
   }
   
 };
 
 //Initialize the js
-document.addEventListener("load", cube.init());
+document.addEventListener("load", qbdoo.init());
