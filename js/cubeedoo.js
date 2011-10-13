c78e35c8f255bb05d2a61d27ff8efb0586ca7a50
Dave Gregory <davidg@v7s.com>
2011-10-13 15:30:39 -0700

    data-values set, but other things broken

--- js/cubeedoo.js
+++ js/cubeedoo.js
@@ -2,16 +2,25 @@ var qbdoo = {
 	currentLevel: 1,
 	currentTheme: "numbers",
 	defaultGameDuration: 120,
-	board: document.querySelector("#main"),
-	cards: board.querySelectorAll("div"),
+	board: document.querySelector("#board"),
+	cards: 16,
+	//cardsEls: this.board.querySelectorAll("div[data-value]:not([data-value=0])"),
 	init: function() {
+	  console.log(this.cards.length);
 	  this.events();
+	  this.setupGame();
+  },
+  setupGame: function() {
+    for (var i=1; i<=this.cards;i++) {
+      this.board.querySelectorAll("div[data-value]")[i-1].setAttribute("data-value", 1);
+      console.log(this.board.querySelectorAll("div[data-value]")[i-1]);
+    }
   },
   events: function() {
     console.log("events()");
-    for (var i=0; i<this.cards.length; i++) {
-      this.cards[i].addEventListener("click", this.turnCard );
-    }
+    //for (var i=0; i<this.cardsEls.length; i++) {
+    //  this.cards[i].addEventListener("click", this.turnCard );
+    //}
   },
   turnCard: function() {
     //turns the card by changing the className
@@ -66,9 +75,9 @@ var qbdoo = {
   renderMenu: function() {
     
   },
-  changeTheme: function(class) {
+  changeTheme: function(klass) {
 	//change the theme by changing the class
-    document.getElementById('game').setAttribute('class',class);
+    document.getElementById('game').setAttribute('class',klass);
   }
   
 };
