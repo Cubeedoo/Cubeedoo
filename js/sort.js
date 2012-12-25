20f11d803f75db50002012a02d4c3f5d27ad0533
Estelle Weyl <estelle@weyl.org>
2012-12-24 20:51:13 -0800

    test worker

---
+++ js/sort.js
@@ -0,0 +1,9 @@
+self.onmessage=function(event){
+		console.log('hi');
+		console.dir(event);
+		console.log(event);
+ 		// handle the message
+		//var stuff = event.data;
+		// and send it back to the main thread
+		//postMessage(stuff);
+};
